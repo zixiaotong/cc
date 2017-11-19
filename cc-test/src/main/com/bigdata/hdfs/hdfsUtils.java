a3d7fe3b447f331988f8b43b9829d984a7dd70f1
shanglei <shanglei@zhanghetianxia.com>
2017-11-19 22:27:18 +0800

    commit

--- cc-test/src/main/com/bigdata/hdfs/hdfsUtils.java
+++ cc-test/src/main/com/bigdata/hdfs/hdfsUtils.java
@@ -1,13 +1,15 @@
 package com.bigdata.hdfs;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URI;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -16,13 +18,69 @@ import org.junit.Test;
  */
 public class hdfsUtils {
 
-    @Test
-    public void test1() throws IOException {
+    FileSystem fs;
+
+    @Before
+    public void init() throws Exception {
         Configuration conf = new Configuration();
         conf.set("fs.defaultFS", "hdfs://master:9000");
-        FileSystem fs = FileSystem.get(conf);
-        FSDataOutputStream os = fs.create(new Path("hdfs://master:9000/nginx_164.tar.gz"));
-        FileInputStream is = new FileInputStream("/Users/shanglei/Downloads/nginx_164.tar.gz");
-        IOUtils.copy(is, os);
+        fs = FileSystem.get(new URI("hdfs://master:9000"), conf, "root");
+    }
+
+    /**
+     * 上传文件到hdfs
+     */
+    @Test
+    public void testUpload() throws IOException {
+        fs.copyFromLocalFile(new Path("/Users/shanglei/Downloads/nginx_164.tar.gz"),
+            new Path("/nginx_164.tar.gz"));
+    }
+
+    /**
+     * 下载文件到本地
+     */
+    @Test
+    public void testDownload() throws IOException {
+        fs.copyToLocalFile(new Path("/nginx_164.tar.gz"),
+            new Path("/Users/shanglei/Downloads/nginx_164_1.tar.gz"));
+    }
+
+    /**
+     * 创建文件夹
+     */
+    @Test
+    public void testMkdir() throws IOException {
+
+        fs.mkdirs(new Path("/a/b/c"));
+    }
+
+    @Test
+    /**
+     * 查看目录信息
+     */
+    public void listFileInfo() throws IOException {
+        RemoteIterator<LocatedFileStatus> remoteIterator = fs.listFiles(new Path("/"), true);
+        while (remoteIterator.hasNext()) {
+            LocatedFileStatus info = remoteIterator.next();
+            System.out.println(info.getPath().getName());
+        }
+
+        System.out.println("--------------------");
+
+        FileStatus[] fileStatuses = fs.listStatus(new Path("/"));
+        for (FileStatus fileStatus : fileStatuses) {
+
+            String str = fileStatus.isDirectory() ? "d--" : "f--";
+
+            System.out.println(str + "---" + fileStatus.getPath().getName());
+        }
+    }
+
+    @Test
+    /**
+     * 修改文件名
+     */
+    public void removeName() throws IOException {
+        fs.rename(new Path("/nginx_164.tar.gz"), new Path("/nginx164.tar.gz"));
     }
 }
