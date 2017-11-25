aa2a31c6b3eba0b681da43fcd4fd240a6776027f
zixiaotong <sljava@126.com>
2017-11-25 14:38:56 +0800

    commit

--- cc-test/src/main/com/bigdata/rpc/PublishService.java
+++ cc-test/src/main/com/bigdata/rpc/PublishService.java
@@ -15,16 +15,11 @@ public class PublishService {
     /**
      * 通过hadoop 的rpc框架来发布一个服务，启动的其实是scoket的server
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Builder builder = new RPC.Builder(new Configuration());
         builder.setBindAddress("master").setPort(10041).setProtocol(IUserLogin.class).
                 setInstance(new UserLoginImpl());
-        Server server = null;
-        try {
-            server = builder.build();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Server server = builder.build();
         server.start();
     }
 }
