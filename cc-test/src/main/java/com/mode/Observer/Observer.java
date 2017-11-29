03ce993f2a291bba4938a74db04d661594f5beca
shanglei <shanglei@zhanghetianxia.com>
2017-11-29 17:26:35 +0800

    修改代码

--- cc-test/src/main/java/com/mode/Observer/Observer.java
+++
@@ -1,8 +0,0 @@
-package com.cn.mode.Observer;//package com.online.cn.com.mode.Observer;
-//
-///**
-// * Created by shanglei on 2017/6/13.
-// */
-//public interface Observer {
-//    void update(Observable o);
-//}
