01231d58297628026c748e71fd9cc9da2942fab4
sljava@126.com <shanglei123>
2017-12-02 18:26:45 +0800

    commit

--- cc-test/src/main/java/com/bigdata/mr/WordCountDriver.java
+++ cc-test/src/main/java/com/bigdata/mr/WordCountDriver.java
@@ -1,12 +1,16 @@
 package com.bigdata.mr;
 
+import java.net.URI;
+
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -120,6 +124,28 @@ public class WordCountDriver {
         wcjob.setOutputValueClass(LongWritable.class);
         FileInputFormat.setInputPaths(wcjob, new Path("/Users/shanglei/Downloads/wordcount/data/"));
         FileOutputFormat.setOutputPath(wcjob, new Path("/Users/shanglei/Downloads/wordcount/output/"));
+        wcjob.waitForCompletion(true);
+    }
+
+    /**
+     * 跑集群模式
+     *
+     * @throws Exception
+     */
+    @Test
+    public void test5() throws Exception {
+        Configuration conf = new Configuration();
+        conf.set("mapreduce.job.jar", "cc-test.jar");
+        Job wcjob = Job.getInstance(conf);
+        wcjob.setJarByClass(WordCountDriver.class);
+        wcjob.setMapperClass(WordCountMapper.class);
+        wcjob.setReducerClass(WordCountReducer.class);
+        wcjob.setMapOutputKeyClass(Text.class);
+        wcjob.setMapOutputValueClass(LongWritable.class);
+        wcjob.setOutputKeyClass(Text.class);
+        wcjob.setOutputValueClass(LongWritable.class);
+        FileInputFormat.setInputPaths(wcjob, new Path("/wordcount/data/"));
+        FileOutputFormat.setOutputPath(wcjob, new Path("/wordcount/output/"));
         wcjob.waitForCompletion(true);
     }
 
