4c54634db7520b1cb4ac40b593b149a673be2049
sljava@126.com <shanglei123>
2017-12-02 17:13:54 +0800

    commit

--- cc-test/src/main/java/com/bigdata/mr/WordCountDriver.java
+++ cc-test/src/main/java/com/bigdata/mr/WordCountDriver.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.junit.Test;
 
 /**
  * @author shanglei
@@ -14,25 +15,86 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
  */
 public class WordCountDriver {
 
+    /**
+     * 集群中运行模式打成jar包
+     *
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job wcjob = Job.getInstance(conf);
-
         wcjob.setJarByClass(WordCountDriver.class);
-
         wcjob.setMapperClass(WordCountMapper.class);
         wcjob.setReducerClass(WordCountReducer.class);
-
         wcjob.setMapOutputKeyClass(Text.class);
         wcjob.setMapOutputValueClass(LongWritable.class);
-
         wcjob.setOutputKeyClass(Text.class);
         wcjob.setOutputValueClass(LongWritable.class);
-
         FileInputFormat.setInputPaths(wcjob, new Path("/wordcount/data/"));
-
         FileOutputFormat.setOutputPath(wcjob, new Path("/wordcount/output/"));
+        wcjob.waitForCompletion(true);
+    }
 
+    /**
+     * 本地只运行程序，数据取的是hdfs上的，计算数据在本地
+     */
+    @Test
+    public void test1() throws Exception {
+        Configuration conf = new Configuration();
+        // 在idea本地run集群中的数据，需要加这一行。否则路径要写全路径（hdfs集群中的全路径）
+        conf.set("fs.defaultFS", "hdfs://master:9000");
+        Job wcjob = Job.getInstance(conf);
+        wcjob.setJarByClass(WordCountDriver.class);
+        wcjob.setMapperClass(WordCountMapper.class);
+        wcjob.setReducerClass(WordCountReducer.class);
+        wcjob.setMapOutputKeyClass(Text.class);
+        wcjob.setMapOutputValueClass(LongWritable.class);
+        wcjob.setOutputKeyClass(Text.class);
+        wcjob.setOutputValueClass(LongWritable.class);
+        FileInputFormat.setInputPaths(wcjob, new Path("/wordcount/data/"));
+        FileOutputFormat.setOutputPath(wcjob, new Path("/wordcount/output/"));
+        wcjob.waitForCompletion(true);
+    }
+
+    /**
+     * 本地只运行程序，数据在本地，计算数据在本地
+     */
+    @Test
+    public void test2() throws Exception {
+        Configuration conf = new Configuration();
+        Job wcjob = Job.getInstance(conf);
+        wcjob.setJarByClass(WordCountDriver.class);
+        wcjob.setMapperClass(WordCountMapper.class);
+        wcjob.setReducerClass(WordCountReducer.class);
+        wcjob.setMapOutputKeyClass(Text.class);
+        wcjob.setMapOutputValueClass(LongWritable.class);
+        wcjob.setOutputKeyClass(Text.class);
+        wcjob.setOutputValueClass(LongWritable.class);
+        FileInputFormat.setInputPaths(wcjob, new Path("/home/wordcount/data/"));
+        FileOutputFormat.setOutputPath(wcjob, new Path("/home/wordcount/output/"));
+        wcjob.waitForCompletion(true);
+    }
+
+    /**
+     * 本地只运行程序，数据取的是hdfs上的，计算数据也在yarn集群上
+     * 需要加四个xml
+     */
+    @Test
+    public void test3() throws Exception {
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs://master:9000");
+        conf.set("mapreduce.job.jar", "cc-test.jar");
+        Job wcjob = Job.getInstance(conf);
+        wcjob.setJarByClass(WordCountDriver.class);
+        wcjob.setMapperClass(WordCountMapper.class);
+        wcjob.setReducerClass(WordCountReducer.class);
+        wcjob.setMapOutputKeyClass(Text.class);
+        wcjob.setMapOutputValueClass(LongWritable.class);
+        wcjob.setOutputKeyClass(Text.class);
+        wcjob.setOutputValueClass(LongWritable.class);
+        FileInputFormat.setInputPaths(wcjob, new Path("/wordcount/data/"));
+        FileOutputFormat.setOutputPath(wcjob, new Path("/wordcount/output/"));
         wcjob.waitForCompletion(true);
     }
 
