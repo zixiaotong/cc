a5331687a32502835483ae43da6ecc94c404a492
shanglei <shanglei123>
2018-04-21 17:16:16 +0800

    commit

---
+++ cc-test/src/main/java/com/bigdata/hive/AreaTransfer.java
@@ -0,0 +1,24 @@
+package com.bigdata.hive;
+
+import java.util.HashMap;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class AreaTransfer extends UDF {
+
+    private static HashMap<String, String> areaMap = new HashMap<>();
+
+    static {
+        areaMap.put("138", "beijing");
+        areaMap.put("139", "nanjing");
+        areaMap.put("137", "tianjin");
+    }
+
+    public String evaluate(String phone) {
+
+        return areaMap.get(phone.substring(0, 3)) == null ? "other" : areaMap
+            .get(phone.substring(0, 3));
+
+    }
+
+}
