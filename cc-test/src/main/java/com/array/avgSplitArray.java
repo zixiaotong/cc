2eac71f9e5f45a6995025791e47b0512af613619
shanglei <shanglei123>
2018-12-20 23:24:57 +0800

    commit

--- cc-test/src/main/java/com/array/avgSplitArray.java
+++ cc-test/src/main/java/com/array/avgSplitArray.java
@@ -1,8 +1,48 @@
 package com.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
 /**
  * @author shanglei
  * @date 2018/12/20 22:19
  */
 public class avgSplitArray {
+    @Test
+    public void testSendMsg() {
+        System.out.println(split(Lists.newArrayList(1,5), 3));
+    }
+
+    public static List<List<Integer>> split(List<Integer> origin, int num) {
+        if (origin == null || origin.size() == 0) {
+            return null;
+        }
+        if (num <= 0) {
+            throw new RuntimeException();
+        }
+        List<Integer> copy = new ArrayList<>(origin);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = num; i > 0; i--) {
+            int length = copy.size();
+            if (length == 0) {
+                result.add(new ArrayList<>());
+                continue;
+            }
+            int tmp = length / i;
+            if (length % i != 0) {
+                tmp++;
+            }
+            result.add(copy.subList(0, tmp));
+            if (tmp < copy.size()) {
+                copy = copy.subList(tmp, copy.size());
+            } else {
+                copy = new ArrayList<>();
+            }
+        }
+        return result;
+    }
 }
